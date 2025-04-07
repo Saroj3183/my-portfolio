9049a4af5cc6b2810945416149ab58a9d3db87a4
Saroj3183 <sarojthakor31@gmail.com>
2025-04-07 16:25:17 +0530

    Added serve dependency for Render Deployment

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import
+import { initializeApp } from "firebase/app";
+
+// Your Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyBzNFSaS5l6U_XKWtiueJVTfoDTWpI-KWM",
+  authDomain: "my-portfolio-93be6.firebaseapp.com",
+  projectId: "my-portfolio-93be6",
+  storageBucket: "my-portfolio-93be6.appspot.com",
+  messagingSenderId: "78314659500",
+  appId: "1:78314659500:web:91a85d908c2a86c2fb0909",
+  measurementId: "G-C21TQ543VV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
